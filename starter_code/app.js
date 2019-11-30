b5a44c0b93d41618a9869d544711923f486e8f87
blablamedia <pierre@gambaud.com>
2019-11-30 10:09:18 +0100

    artists search and display done

--- starter_code/app.js
+++ starter_code/app.js
@@ -2,10 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
-
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 
@@ -13,16 +10,49 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-
-// setting the spotify-api goes here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 
-
+// spotify API
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+  
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  })
+  .catch(error => {
+    console.log("Something went wrong when retrieving an access token", error);
+  });
 
 
 // the routes go here:
+app.get(`/`, function(req, res, next) {
+  res.render(`index`);
+})
 
+app.get(`/artists`, function(req, res, next) {
+  // spotify API
+  spotifyApi
+  .searchArtists(req.query.q)
+  .then(data => {
+    // collect artists
+    if(data.body.artists.items[0].images.length !== 0) {
+      console.log(data.body.artists.items[0])
+    }
+    
+    res.render(`artists`, {
+      artists: data.body.artists.items
+    });
+  })
+  .catch(err => {
+    console.log("The error while searching artists occurred: ", err);
+  });
+})
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
