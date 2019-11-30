5df494397874b07113b83f482c5678d34dc4636d
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-11-30 14:09:38 +0100

    Iteration 5 + styling

--- starter_code/app.js
+++ starter_code/app.js
@@ -47,7 +47,9 @@ app.get('/artists', (req,res,next)=>{
   .then(data => {
     //console.log("The received data from the API: ", data.body);
     let artists=data.body.artists.items.map(artist=>{
-      return {artistName:artist.name, artistPicture:artist.images[1], artistId:artist.id}; 
+      let img=artist.images; 
+      img.length>0? img=artist.images[1].url:img="/images/defaultArtistImage.png";
+      return {artistName:artist.name, artistPicture:img, artistId:artist.id}; 
     })
     res.render('artistResult', {artists});
     //res.send(data.body);
@@ -63,16 +65,29 @@ app.get('/albums/:artistId', (req,res,next)=>{
   .then(function(data) {
     console.log('Artist albums', data);
     let albums=data.body.items.map(album=>{
-      return {albumName:album.name, albumImage:album.images[1], albumId:album.id}
+      let img=album.images; 
+      img.length>0? img=album.images[1].url:img="/images/defaultAlbumImage.png";
+      return {albumName:album.name, albumImage:img, albumId:album.id};
     })
     res.render('albumResult', {albums})
-    //res.send(data)
+    //res.send(albums)
   }, function(err) {
     console.error(err);
   });
 })
 
-app.get('/albums/:albumId/tracks', (res,req,next)=>{
+app.get('/tracks/:albumId', (req,res,next)=>{
+   spotifyApi.getAlbumTracks(req.params.albumId)
+  .then(function(data) {
+    let tracks=data.body.items.map(track=>{
+      
+      return {trackName:track.name, trackPrev:track.preview_url};
+    })
+    res.render('trackResult', {tracks}); 
+
+  }, function(err) {
+    console.log('Something went wrong when getting album tracks!', err);
+  });
   
 })
 
